0594484fb50235666c0ccaaeed5ed6b4110d8af3
nina <ninaw2306@gmail.com>
2024-01-24 10:55:50 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data(){
+    return{
+      title: 'deploy-demo02',
+      text: 'use npm gh-pages'
+    }
+  }
+}
+</script>
+
+<template>
+  <div style="display: flex; flex-direction: column; justify-content: center;">
+    <h1>{{ title }}</h1>
+    <p>{{ text }}</p>
+  </div>
+</template>
